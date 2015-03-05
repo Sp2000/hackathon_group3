e19254244bda6891d6dda74e6df48ba710ce77aa
maarten schermer <maarten.schermer@naturalis.nl>
2015-03-05 12:38:07 +0100

    And again....

--- web/includes/config.php
+++ web/includes/config.php
@@ -8,7 +8,6 @@
         define('SPECIES_PER_PAGE',100);
         define('CHECKLIST_SPECIES_ID_PREFIX','http://www.nederlandsesoorten.nl/nsr/concept/');
         //define('INCOMING_DATA_FOLDER','C:\www\hackathon2015\hackaton_group3\incoming\\');
-        //define('INCOMING_DATA_FOLDER','/var/www/htdocs/group_3/hackathon_group3/incoming/');
-        define('INCOMING_DATA_FOLDER','/root/group_3/');
+        define('INCOMING_DATA_FOLDER','/var/www/htdocs/group_3/hackathon_group3/incoming/');
 
         $ACCEPTED_PROVIDERS=['GBIF','CoL'];
